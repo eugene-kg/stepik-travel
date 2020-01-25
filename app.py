e5b815244f0ed271928434de7f0e9b15804ecf69
eugene-kg <eugenekagan@gmail.com>
2020-01-26 00:05:30 +0200

    Initial commit
    Signed-off-by: eugene-kg <eugenekagan@gmail.com>

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import data as tours_data
+
+app = Flask(__name__)
+
+
+# Main page
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+# Directions
+@app.route('/from/<direction>/')
+def get_direction(direction):
+    return render_template('direction.html', direction=direction)
+
+
+# Tour
+@app.route('/tours/<id>/')
+def tour(id):
+    tours = tours_data.tours
+    departures = tours_data.departures
+    return render_template('tour.html', id=id, tours=tours)
+
+
+app.run()
